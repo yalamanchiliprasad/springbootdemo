22ffc315a57b528fd3eb28f9a8098b2e13bc772f
yalamanchiliprasad <50310727+yalamanchiliprasad@users.noreply.github.com>
2025-09-03 05:32:27 +0530

    Update TestDemo.java

--- springbootdemo/src/main/java/springbootdemo/TestDemo.java
+++ springbootdemo/src/main/java/springbootdemo/TestDemo.java
@@ -4,7 +4,7 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hi");
+       System.out.println("Hi how are you");
        System.out.println("Hello");
 	}
 
